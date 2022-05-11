3476e1ff24318ffbff15357f6ecb653ae7390fe3
s285697 <s285697@shhac150M17.ocsb.local>
2022-05-11 12:18:10 -0400

    making a github duplicate

---
+++ animal.py
@@ -0,0 +1,28 @@
+
+x = 2 
+print(x)
+
+y = float(x) 
+
+print(y) 
+
+z = input("please enter a number") 
+print(z) 
+
+z = int(z) 
+
+print(z)
+
+
+
+
+answer = 5/ 2  # division
+
+answer2 = 5// 2 # integer division
+
+answer3 = 5% 2 # odulus, give remainder
+
+
+print(answer)
+print(answer2)
+print(answer3)
--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 print("Hello world")
 
-print("Something in the way uhmmmmuhmmuhm")
