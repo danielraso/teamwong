352bcc79eb832dc2a88f9c287f7ff07ab71c915b
danielraso <104920216+danielraso@users.noreply.github.com>
2022-05-11 12:24:39 -0400

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,23 @@
+"""
+for counter in range (3, 5):
+    print(counter)
+    print("hello")
+    
+    
+myName = "slimshady"
+
+for letter in myName:
+    print(letter)
+"""
+
+"""
+while 1==1:
+    print("hello")
+"""
+
+
+userInput = int(input("pleas enter a number")
+
+while count < userInput:
+    print("hello world!")
+    count = count + 1
