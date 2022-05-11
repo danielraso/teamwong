414452ea266905747e6de081bb94cb1912eed8cc
danielraso <104920216+danielraso@users.noreply.github.com>
2022-05-11 12:22:20 -0400

    Create decision structure.py

---
+++ decision structure.py
@@ -0,0 +1,63 @@
+#print("I frew up") 
+
+#print("placing a hashtag in front of a word will make it a comment, it still shows up in the sandbox but not the console")
+
+'''
+If you put the code between e single uotes it blocks the code
+''' 
+
+"""
+Same thing with 3 double removeQuotes
+"""
+
+"""
+A conditon is a comparison 
+Contitions evaluate a boolean value to be true or false 
+if a condition is true, the folloing block of code wil, run 
+a block of code willbe  intended 
+"""
+'''
+comparisons:
+> grater than 
+< less than 
+>= greater then or equal to 
+<= less than or equal to 
+== equal to 
+!= not eual to 
+'''
+
+mark = int(input("please enter your test mark"))
+
+if mark >= 50 and mark < 60: 
+    print("you passed somehow. You get a D")
+    
+    
+if mark < 50 and mark > 0:
+    print("you failed")
+    print("c'mon man")
+
+if mark >= 60 and mark <70:
+    print("Good job, you get a C! The coolest mark")
+
+if mark >= 70 and mark < 80:
+    print("thats a B, it's average but average won't get you into Harvard loser") 
+    
+if mark >= 80 and mark < 90:
+    print("You get an A, wowwwwwwww")
+
+if mark >= 90 and mark < 100:
+    print("atta b champ")
+    print("you aced it!")
+    print("A+")
+    
+if mark == 100:
+    print("cheater") 
+    
+if mark == 0: 
+    print:("Yikes")
+    
+if mark < 0:
+    print("thats not a possible score silly")
+    
+if mark > 100: 
+    print("not funny didn't laugh")
