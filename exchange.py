313ace8ff10359c7d5e097a88489643e5e70be8c
danielraso <104920216+danielraso@users.noreply.github.com>
2022-05-11 12:23:09 -0400

    Create exchange.py

---
+++ exchange.py
@@ -0,0 +1,24 @@
+from ctypes.wintypes import HMONITOR
+x = "monitor" 
+
+print(x)
+print(len(x))
+print(x[0])
+print(x[-1])
+print(x[1:6])
+
+
+
+
+
+print("foo" + " " + "bar") 
+
+
+
+
+y = x[-1] + x[1:6]
+
+
+
+
+print(y)
