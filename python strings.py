d86089d29d38a66a6dc8924865f53ad14b1647ac
danielraso <104920216+danielraso@users.noreply.github.com>
2022-05-11 12:28:10 -0400

    Create python strings.py

---
+++ python strings.py
@@ -0,0 +1,20 @@
+"""
+all strings are a datatype
+strings can be changed
+strings are writen ith a single or double uotes 
+All user inputs are strings
+"""
+
+string1 = "This is a string"
+string2 = "This is also a string"
+
+print("string1")
+print("string2")
+
+#var1 = input("please enter some text")
+
+#print("you entered" + var1)
+
+print(string1)
+print(string1[3:6])
+print(string1[6:7] * 92)
